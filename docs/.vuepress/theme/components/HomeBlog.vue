1b5c34123be64bf39cfffaf6c7c8ca2ace6f4f69
魏闯 <15944699432@163.com>
2020-06-11 11:28:53 +0800

    genxin

--- docs/.vuepress/theme/components/HomeBlog.vue
+++ docs/.vuepress/theme/components/HomeBlog.vue
@@ -88,9 +88,7 @@ export default {
   created() { //生命周期函数
 		//判断当前页面是否被隐藏
 		var that = this;
-		var hiddenProperty = 'hidden' in document ? 'hidden' :
-			'webkitHidden' in document ? 'webkitHidden' :
-			'mozHidden' in document ? 'mozHidden' :	null;
+		var hiddenProperty = 'hidden' in document ? 'hidden' :'webkitHidden' in document ? 'webkitHidden' :'mozHidden' in document ? 'mozHidden' :	null;
 		var visibilityChangeEvent = hiddenProperty.replace(/hidden/i, 'visibilitychange');
 		var onVisibilityChange = function() {
 			if (document[hiddenProperty]) { //被隐藏
